1f553770eca76779f7b5b9b65504b12e3a7adbad
DBCKS97 <wonyu1997@naver.com>
2025-09-30 16:43:16 +0900

    Class 설계
    아이디어에서 명사만 뽑아 클래스로 만들고, 클래스(명사)간 관계고려

--- main.cpp
+++ main.cpp
@@ -1,52 +1,101 @@
 ﻿#include <iostream>
-
 using namespace std;
 
-/*
- 테이블 -> C++
- Character
- ID Name Str Dex
- 0   A    2
- 1   B    3
- 2   C    2
-
- // 데이터 모델링
- class Character
- {
-	int ID;
-	string Name;
-	int Str;
-	int Dex;
- }
- */
-
-struct FRGB
-{
-	int R = 0;
-	int G = 0;
-	int B = 0;
-};
-struct FCoord
-{
-	int X = 0;
-	int Y = 0;
-};
-
-class Image
+class Engine
 {
 public:
-	FRGB RGB;
-	FCoord Coord;
+	void Run()
+	{
+		while (true)
+		{
+			Input();
+			Tick();
+			Render();
+		}
+	}
+
+	void Input()
+	{
+
+	}
+
+	void Tick()
+	{
+
+	}
+
+	void Render()
+	{
+
+	}
+
+	World* 
+};
+
+class World
+{
+public:
+	void GameOver()
+	{
+
+	}
+
+	AWall* Wall = nullptr;
+	AFloor* Floor = nullptr;
+
+	APlayer* Player = nullptr;
+	AMonster* Monster = nullptr;
+
+	ADestination Destination;
+};
+
+class APlayer
+{
+public:
+	void Move()
+	{
+		
+	}
+
+	int PositionX;
+	int PositionY;
+};
+
+class AMonster
+{
+public:
+	void Move()
+	{
+
+	}
+
+	int PositionX;
+	int PositionY;
+};
+
+class AWall
+{
+public:
+	int SizeX = 0;
+	int SizeY = 0;
+};
+
+class AFloor
+{
+public:
+	int SizeX = 0;
+	int SizeY = 0;
+};
+
+class ADestination
+{
+public:
+	int PositionX;
+	int PositionY;
 };
 
 int main()
 {
-	Image* image = new Image;
-	image->RGB.R = 1.f;
-	image->RGB.G = 0.f;
-	image->RGB.B = 0.f;
-
-
+	
 	return 0;
 }
-
