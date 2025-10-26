83c23b97343d66c3ea0056599994191114f1f006
yuyu8488 <wonyu1997@naver.com>
2025-10-27 01:14:10 +0900

    .

--- main.cpp
+++ main.cpp
@@ -1,21 +1,35 @@
 ﻿#include "FEngine.h"
 #include <vector>
 #include <iostream>
+#include <Windows.h>
+
 #include "DynamicArray.h"
 #include "Math.h"
 
-int main(int argc, char* argv[])
+DWORD WINAPI SecondThread(PVOID pvParam)
 {
-	Math::Matrix4x4 Matrix(
-		1.f, 0.f, 0.f, 0.f,
-		0.f, 1.f, 0.f, 0.f,
-		0.f, 0.f, 1.f, 0.f,
-		0.f, 0.f, 0.f, 1.f);
-	
+	*(int*)pvParam = 5;
 	
 
-	
-	
+	return NULL;
+}
+
+DWORD WINAPI FirstThread(PVOID pvParam)
+{
+	int x =  (int)pvParam;
+	DWORD dwThreadID;
+
+	HANDLE hThread = CreateThread(NULL, 0, SecondThread, (PVOID)&x, 0, &dwThreadID);
+
+	CloseHandle(hThread);
+
+	return(0);
+}
+
+int main(int argc, char* argv[])
+{
+	int a = 3;
+	FirstThread((PVOID)&a);
 
 
 	return 0;
